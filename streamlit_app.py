d462f045fbc7637da39341c0b0f4dce140049a1e
darkhira <105603531+darkhira@users.noreply.github.com>
2024-04-02 15:48:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,241 +1,153 @@
+
 import streamlit as st
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import confusion_matrix, accuracy_score, roc_curve, auc
-import seaborn as sns
-import pickle
-from sklearn.impute import SimpleImputer
-import altair as alt
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import plot_tree
+import seaborn as sn
+import joblib as jb
+
+
+@st.cache_resource
+def load_models():
+  modeloNB=jb.load('modeloNB.bin')
+  modeloArbol=jb.load('ModeloArbol.bin')
+  modeloBosque=jb.load('ModeloBosque.bin')
+  return modeloNB,modeloArbol,modeloBosque
+
+
+modeloNB,modeloArbol,modeloBosque= load_models()
+
+st.title("Aplicación de predicción")
+st.header('Trabajo-Data-Science', divider='rainbow')
+st.subheader('Ejemplo en los modelos :blue[Arbol de Decisión, Bosque Aleatorio y Naive Bayes]')
+
+
+with st.container( border=True):
+  st.subheader("Modelo Machine Learning para predecir la deserción de clientes")
+  st.write("""
+
+**Introducción**
+cliente de nuestros sueños es el que permanece fiel a la empresa, comprando siempre sus productos o servicios. Sin embargo, en la realidad,
+los clientes a veces deciden alejarse de la empresa para probar o empezar a comprar otros productos o servicios y esto puede ocurrir en
+cualquier fase del customer journey. Sin embargo, existen varias medidas para prevenir o gestionar mejor esta circunstancia. Por eso lo mejor
+es tener una herramienta predictiva que nos indique el estado futuro de dichos clientes usando inteligencia artificial, tomar las acciones
+de retenció necesaria. Constituye pues esta aplicación una herramienta importante para la gestión del marketing.
+
+Los datos fueron tomados con la Información de la base de datos CRM de la empresa ubicada en Bucaramanfa,donde se
+preparó 3 modelos de machine Learnig para predecir la deserció de clientes, tanto actuales como nuevos.
+
+Datos Actualizados en la fuente: 20 de Marzo del 2024
+
+
+Se utilizó modelos supervidados de clasificacion  tanto Naive Bayes, Arboles de decisión y Bosques Aleatorios
+entendiendo que hay otras técnicas, es el resultado de la aplicacion practico del curso de inteligencia artificial en estos modelos
+revisado en clase. Aunqe la aplicación final sería un solo modelo, aqui se muestran los tres modelos para
+comparar los resultados.
+
+ """)
+
+
+
+modeloA=['Naive Bayes', 'Arbol de Decisión', 'Bosque Aleatorio']
+
+churn = {1 : 'Cliente se retirará', 0 : 'Cliente No se Retirará' }
+
+
+styleimagen ="<style>[data-testid=stSidebar] [data-testid=stImage]{text-align: center;display: block;margin-left: auto;margin-right: auto;width: 100%;}</style>"
+st.sidebar.markdown(styleimagen, unsafe_allow_html=True)
+
+
+styletexto = "<style>h2 {text-align: center;}</style>"
+st.sidebar.markdown(styletexto, unsafe_allow_html=True)
+st.sidebar.header('Seleccione los datos de entrada')
+
+
+def seleccionar(modeloL):
+
+
+  st.sidebar.subheader('Selector de Modelo')
+  modeloS=st.sidebar.selectbox("Modelo",modeloL)
+
+  st.sidebar.subheader('Seleccione la COMP')
+  COMPS=st.sidebar.slider("Seleccion",4000,18000,8000,100)
+
+  st.sidebar.subheader('Selector del PROM')
+  PROMS=st.sidebar.slider("Seleccion",   0.7, 9.0,5.0,.5)
+
+  st.sidebar.subheader('Selector de COMINT')
+  COMINTS=st.sidebar.slider("Seleccione",1500,58000,12000,100)
+
+  st.sidebar.subheader('Selector de COMPPRES')
+  COMPPRESS=st.sidebar.slider('Seleccione', 17000,90000,25000,100)
+
+  st.sidebar.subheader('Selector de RATE')
+  RATES=st.sidebar.slider("Seleccione",0.5,4.2,2.0,0.1)
+
+  st.sidebar.subheader('Selector de DIASSINQ')
+  DIASSINQS=st.sidebar.slider("Seleccione", 270,1800,500,10)
+
+  st.sidebar.subheader('Selector de TASARET')
+  TASARETS=st.sidebar.slider("Seleccione",0.3,1.9,0.8,.5)
+
+  st.sidebar.subheader('Selector de NUMQ')
+  NUMQS=st.sidebar.slider("Seleccione",3.0,10.0,4.0,0.5)
+
+  st.sidebar.subheader('Selector de RETRE entre 3 y 30')
+
+  RETRES=st.sidebar.number_input("Ingrese el valor de RETRE", value=3.3, placeholder="Digite el numero...")
+
+  return modeloS,COMPS, PROMS, COMINTS ,COMPPRESS, RATES, DIASSINQS,TASARETS, NUMQS, RETRES
+
+
+
+modelo,COMP, PROM, COMINT ,COMPPRES, RATE, DIASSINQ,TASARET, NUMQ, RETRE=seleccionar(modeloA)
+
+
+with st.container(border=True):
+  st.subheader("Predición")
+  st.title("Predicción de Churn")
+  st.write(""" El siguiente es el pronóstico de la deserción usando el modelo
+           """)
+  st.write(modelo)
+  st.write("Se han seleccionado los siguientes parámetros:")
+  # Presento los parámetros seleccionados en el slidder
+  lista=[[COMP, PROM, COMINT ,COMPPRES, RATE, DIASSINQ,TASARET, NUMQ, RETRE]]
+  X_predecir=pd.DataFrame(lista,columns=['COMP', 'PROM', 'COMINT', 'COMPPRES', 'RATE', 'DIASSINQ','TASARET', 'NUMQ', 'RETRE'])
+  st.dataframe(X_predecir)
 
 
 
 
-# Cargar el conjunto de datos desde una URL
-url = 'https://raw.githubusercontent.com/adiacla/bigdata/master/DatosEmpresaChurn.csv'
-df = pd.read_csv(url)
-
-# Convertir etiquetas de clase a valores enteros binarios
-le = LabelEncoder()
-df['TARGET CLASS'] = le.fit_transform(df['TARGET CLASS'])
-y = df['TARGET CLASS']  # Definir y después de la conversión
-
-# Sidebar para aceptar parámetros de entrada
-with st.sidebar:
-    st.header('Descargar CSV')
-    st.write('Haz clic en el siguiente botón para descargar el archivo CSV.')
-    csv_data = df.to_csv(index=False).encode('utf-8')
-    st.download_button("DatosEmpresaChurn", csv_data, mime='text/csv', file_name='DatosEmpresaChurn.csv')
-
+  if modelo=='Naive Bayes':
+      y_predict=modeloNB.predict(X_predecir)
+      probabilidad=modeloNB.predict_proba(X_predecir)
+      importancia=pd.DataFrame()
+  elif modelo=='Arbol de Decisión':
+      y_predict=modeloArbol.predict(X_predecir)
+      probabilidad=modeloArbol.predict_proba(X_predecir)
+      importancia=modeloArbol.feature_importances_
+      features=modeloArbol.feature_names_in_
+  else :
+      y_predict=modeloBosque.predict(X_predecir)
+      probabilidad=modeloBosque.predict_proba(X_predecir)
+      importancia=modeloBosque.feature_importances_
+      features=modeloBosque.feature_names_in_
 
 
-with st.expander('Naive Bayes'):
- if 'df' in locals():
-    # Imputando los valores NaN con la media de la columna
-    df = df.replace(',', '.', regex=True)
-    df = df.astype(float)
-    imputer = SimpleImputer(strategy='mean')
-    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+  styleprediccion= '<p style="font-family:sans-serif; color:Green; font-size: 42px;">La predicción es</p>'
+  st.markdown(styleprediccion, unsafe_allow_html=True)
+  prediccion='Resultado: '+ str(y_predict[0])+ "    - en conclusion :"+churn[y_predict[0]]
+  st.header(prediccion+'   :warning::warning::warning:')
 
-    # Codificando las columnas categóricas
-    le = LabelEncoder()
-    for column in df.columns:
-        if df[column].dtype == object:
-            df[column] = le.fit_transform(df[column])
+  st.write("Con la siguiente probabilidad")
 
-    # Normalizando el conjunto de datos
-    scaler = StandardScaler()
-    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+  col1, col2= st.columns(2)
+  col1.metric(label="Probalidad de NO :", value="{0:.2%}".format(probabilidad[0][0]),delta=" ")
+  col2.metric(label="Probalidad de SI:", value="{0:.2%}".format(probabilidad[0][1]),delta=" ")
 
-    # Dividiendo el conjunto de datos en conjuntos de entrenamiento y prueba
-    X = df.drop('TARGET CLASS', axis=1)
-    y = df['TARGET CLASS']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-    # Convertir etiquetas de clase a valores enteros binarios
-    y_train = y_train.astype(int)
-    y_test = y_test.astype(int)
-
-    # Creando y entrenando el modelo de Naive Bayes Gaussiano
-    modeloNB = GaussianNB()
-    modeloNB.fit(X_train, y_train)
-
-    # Haciendo predicciones con el modelo
-    y_pred = modeloNB.predict(X_test)
-
-    # Creando una matriz de confusión para evaluar las predicciones
-    cm = confusion_matrix(y_test, y_pred)
-
-    # Visualizando la matriz de confusión
-    st.subheader("Confusion Matrix")
-    st.write(cm)
-
-    # Calculando la precisión del modelo
-    accuracy = accuracy_score(y_test, y_pred)
-
-    # Imprimiendo la precisión del modelo
-    st.subheader("Model Accuracy")
-    st.write(f'La precisión del modelo es: {accuracy}')
-
-    # Haciendo predicciones con el modelo
-    y_pred_proba_NB = modeloNB.predict_proba(X_test)[::, 1]
-
-    # Calculando y dibujando la curva ROC
-    fpr, tpr, _ = roc_curve(y_test, y_pred_proba_NB)
-    auc_score = auc(fpr, tpr)
-    st.subheader("ROC Curve")
-    st.write(f"AUC = {auc_score:.3f}")
-
-    # Mostrar la curva ROC
-    st.altair_chart(alt.Chart(pd.DataFrame({'false_positive_rate': fpr, 'true_positive_rate': tpr})).mark_line().encode(
-        x='false_positive_rate',
-        y='true_positive_rate'
-    ).properties(
-        width=500,
-        height=300
-    ))
-with st.expander('Arboles de decision'):
- if 'df' in locals():
-    #Arboles de Decision
-    df = df.replace(',', '.', regex=True)
-    df = df.astype(float)
-    imputer = SimpleImputer(strategy='mean')
-    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-
-    # Codificando las columnas categóricas
-    le = LabelEncoder()
-    for column in df.columns:
-        if df[column].dtype == object:
-            df[column] = le.fit_transform(df[column])
-
-    # Normalizando el conjunto de datos
-    scaler = StandardScaler()
-    df = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
-
-    # Dividiendo el conjunto de datos en conjuntos de entrenamiento y prueba
-    X = df.drop('TARGET CLASS', axis=1)
-    y = df['TARGET CLASS']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-    # Convertir etiquetas de clase a valores enteros binarios
-    y_train = y_train.astype(int)
-    y_test = y_test.astype(int)
-
-    # Creando y entrenando el modelo de árbol de decisión
-    modeloArbol = DecisionTreeClassifier(random_state=0)
-    modeloArbol.fit(X_train, y_train)
-
-    # Haciendo predicciones con el modelo
-    y_pred = modeloArbol.predict(X_test)
-
-    # Creando una matriz de confusión para evaluar las predicciones
-    cm = confusion_matrix(y_test, y_pred)
-
-    # Visualizando la matriz de confusión
-    st.subheader("Confusion Matrix")
-    st.write(cm)
-
-    # Calculando la precisión del modelo
-    accuracy = accuracy_score(y_test, y_pred)
-
-    # Imprimiendo la precisión del modelo
-    st.subheader("Model Accuracy")
-    st.write(f'La precisión del modelo es: {accuracy}')
-
-    # Haciendo predicciones con el modelo
-    y_pred_proba_arb = modeloArbol.predict_proba(X_test)[::, 1]
-
-    # Calculando y dibujando la curva ROC
-    fpr, tpr, _ = roc_curve(y_test, y_pred_proba_arb)
-    auc_score = auc(fpr, tpr)
-    st.subheader("ROC Curve")
-    st.write(f"AUC = {auc_score:.3f}")
-
-    # Mostrar la curva ROC
-    st.altair_chart(alt.Chart(pd.DataFrame({'false_positive_rate': fpr, 'true_positive_rate': tpr})).mark_line().encode(
-        x='false_positive_rate',
-        y='true_positive_rate'
-    ).properties(
-        width=500,
-        height=300
-    ))
-
-with st.expander('Bosques Aleatorios'):
-    if 'df' in locals():
-        # Preprocesamiento de datos
-        df = df.replace(',', '.', regex=True)
-        df = df.astype(float)
-        imputer = SimpleImputer(strategy='mean')
-        df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
-
-        # Convertir las etiquetas de clase a valores enteros
-        le = LabelEncoder()
-        df['TARGET CLASS'] = le.fit_transform(df['TARGET CLASS'])
-
-        # Normalización de los datos
-        scaler = StandardScaler()
-        X = scaler.fit_transform(df.drop('TARGET CLASS', axis=1))
-        y = df['TARGET CLASS']
-
-        # División del conjunto de datos en entrenamiento y prueba
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-
-        # Creación y entrenamiento del modelo de Bosques Aleatorios
-        modeloBosque = RandomForestClassifier(n_estimators=10,
-                                               criterion="gini",
-                                               bootstrap=True,
-                                               max_features="sqrt",
-                                               max_samples=0.8,
-                                               oob_score=True,
-                                               random_state=0)
-        modeloBosque.fit(X_train, y_train)
-
-        # Predicciones con el modelo
-        y_pred = modeloBosque.predict(X_test)
-
-        # Matriz de confusión
-        cm = confusion_matrix(y_test, y_pred)
-
-        # Visualización de la matriz de confusión
-        st.subheader("Confusion Matrix")
-        st.write(cm)
-
-        # Precisión del modelo
-        accuracy = accuracy_score(y_test, y_pred)
-
-        # Imprimiendo la precisión del modelo
-        st.subheader("Model Accuracy")
-        st.write(f'La precisión del modelo es: {accuracy}')
-
-        # Predicciones de probabilidad
-        y_pred_proba_bos = modeloBosque.predict_proba(X_test)[::,1]
-
-        # Calculando y dibujando la curva ROC
-        fpr, tpr, _ = roc_curve(y_test, y_pred_proba_bos)
-        auc_score = auc(fpr, tpr)
-        st.subheader("ROC Curve")
-        st.write(f"AUC = {auc_score:.3f}")
-
-        # Mostrar la curva ROC
-        st.altair_chart(alt.Chart(pd.DataFrame({'false_positive_rate': fpr, 'true_positive_rate': tpr})).mark_line().encode(
-            x='false_positive_rate',
-            y='true_positive_rate'
-        ).properties(
-            width=500,
-            height=300
-        ))
-
-        # Visualizando algunos (NO TODOS) árboles del bosque aleatorio
-        st.subheader("Visualización de algunos árboles del Bosque Aleatorio")
-        for i, tree_in_forest in enumerate(modeloBosque.estimators_):
-            if i < 5:  # Visualizar solo los primeros 5 árboles
-                fig, ax = plt.subplots(figsize=(10, 7))  # Crear una nueva figura
-                plot_tree(tree_in_forest, filled=True, feature_names=df.columns[:-1], ax=ax)  # Pasar ax=ax
-                st.pyplot(fig)  # Pasar la figura a st.pyplot()
+  st.write("La importancia de cada Factor en el modelo es:")
+  if modelo!='Naive Bayes':
+    importancia=pd.Series(importancia,index=features)
+    st.bar_chart(importancia)
+  else:
+    st.write("Naive Bayes no tiene parámetro de importancia de los features")
